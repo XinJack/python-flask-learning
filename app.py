0a3be14803bfe1276f6e682a6476c330d30a5359
XinJack <1055385195@qq.com>
2017-04-20 20:09:07 +0800

    Add codes

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session, flash
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form 
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[Required()])
+	submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	form = NameForm()
+	if form.validate_on_submit():
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
